ad8ee461d88929547a8a66f3df1783cb3fa97b45
AdityaChatterjee-Dev <chatterjeeadi9@gmail.com>
2026-03-16 21:38:39 +0530

    Update Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,14 @@
 function Header() {
   return (
 <div>
-<h1>Student Dashboard</h1>
+<center>
+  <div style={{backgroundColor: "darkblue", color:"white"}}>
+    <h1>
+      UNITED COLLEGE OF ENGINEERING OF RESEARCH-[2025-2026]
+    </h1>
+    </div>
+  <h1>Student Dashboard</h1>
+</center>
 <hr />
 </div>
   );
